97efae9c2c3e9c31a5a2086fb45cf6dcfe5f9805
Moin Vohra <mvoh0003@student.monash.edu>
2025-09-03 18:19:26 +1000

    Initial Commit

---
+++ FIT3178-ShelfLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIT3178-ShelfLife
+//
+//  Created by Moin Vohra on 3/9/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
